84d16867e0e312a8846281db9757ffee759dc987
sujay2306 <sujayks007@gmail.com>
2023-08-20 09:15:34 +0530

    added color package

--- cmd/trace.go
+++ cmd/trace.go
@@ -1,47 +1,89 @@
 package cmd
 
 import (
+	"encoding/json"
 	"fmt"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"io/ioutil"
+
+	"github.com/fatih/color"
 	"github.com/spf13/cobra"
 )
 
+// traceCmd represents the trace command
 var traceCmd = &cobra.Command{
-		Use:   "trace",
-		Short: "Trace the IP",
-		Long: `Trace the IP"`,
-
-		Run: func(cmd *cobra.Command, args []string) {
-			if len(args) > 0 {
-				for _,ip := range args{
-					fmt.Println(ip)
-				}
-			}else{
-				fmt.Println("Please provide IP Address to trace")
+	Use:   "trace",
+	Short: "Trace the IP",
+	Long:  `Trace the IP.`,
+	Run: func(cmd *cobra.Command, args []string) {
+		if len(args) > 0 {
+			for _, ip := range args {
+				showData(ip)
 			}
-		},
-	}
+		} else {
+			fmt.Println("Please provide IP to trace.")
+		}
+	},
+}
 
-func init(){
+func init() {
 	rootCmd.AddCommand(traceCmd)
 }
 
-func showData(){
-	url := "http://ipinfo.io/1.1.1.1/geo"
-	getData(url)
+// {
+//     "ip": "1.1.1.1",
+//     "hostname": "one.one.one.one",
+//     "anycast": true,
+//     "city": "Miami",
+//     "region": "Florida",
+//     "country": "US",
+//     "loc": "25.7867,-80.1800",
+//     "org": "AS13335 Cloudflare, Inc.",
+//     "postal": "33132",
+//     "timezone": "America/New_York",
+//     "readme": "https://ipinfo.io/missingauth"
+// }
+
+type Ip struct {
+	IP       string `json:"ip"`
+	City     string `json:"city"`
+	Region   string `json:"region"`
+	Country  string `json:"country"`
+	Loc      string `json:"loc"`
+	Timezone string `json:"timezone"`
+	Postal   string `json:"postal"`
 }
 
-func getData(url string) [] byte{
+func showData(ip string) {
+	url := "http://ipinfo.io/" + ip + "/geo"
+	responseByte := getData(url)
+
+	data := Ip{}
+
+	err := json.Unmarshal(responseByte, &data)
+	if err != nil {
+		log.Println("Unable to unmarshal the response")
+	}
+
+	c := color.New(color.FgRed).Add(color.Underline).Add(color.Bold)
+	c.Println("DATA FOUND :")
+
+	fmt.Printf("IP :%s\nCITY :%s\nREGION :%s\nCOUNTRY :%s\nLOCATION :%s\nTIMEZONE:%s\nPOSTAL :%s\n", data.IP, data.City, data.Region, data.Country, data.Loc, data.Timezone, data.Postal)
+
+}
+
+func getData(url string) []byte {
+
 	response, err := http.Get(url)
-	if err != nil{
-		log.Println("unable to get the response")
+	if err != nil {
+		log.Println("Unable to get the response")
 	}
 
 	responseByte, err := ioutil.ReadAll(response.Body)
-	if err != nil{
-		log.Panicln("Unable to read response")
+	if err != nil {
+		log.Println("Unable to read the response")
 	}
+
 	return responseByte
 }
